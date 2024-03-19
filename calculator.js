4960055f13bfc9ca0c100bd37243f79d4fe8c655
Andrei <142443853+hjzres@users.noreply.github.com>
2024-03-19 12:11:35 -0400

    Made the calculator able to put numbers on the top

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
 let total = 0;
+let nums = [];
 
 const clearButton = document.getElementById("clear");
 const negativeButton = document.getElementById("negative");
@@ -12,10 +13,24 @@ const fourButton = document.getElementById("four");
 const fiveButton = document.getElementById("five");
 const sixButton = document.getElementById("six");
 const subtractButton = document.getElementById("subtract");
-const oneButton = document.getElementById("one");
+const oneButton = document.getElementById('one');
 const twoButton = document.getElementById("two");
 const threeButton = document.getElementById("three");
 const addButton = document.getElementById("add");
 const zeroButton = document.getElementById("zero");
 const decimalButton = document.getElementById("decimal");
 const equalButton = document.getElementById("equal");
+
+const numberButtons = document.querySelectorAll('.number');
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        changeNumber(parseInt(button.textContent));
+    });
+});
+
+let changeNumber = (x)=>{
+    total *= 10;
+    total += x;
+    document.getElementById("total").innerHTML = total;
+}
